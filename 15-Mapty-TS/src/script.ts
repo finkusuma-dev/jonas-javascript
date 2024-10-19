77a1aadc09c678075b8283b8fd9e9fcd3e6b72e9
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-10-19 17:47:44 +0700

    replace project files

---
+++ 15-Mapty-TS/src/script.ts
@@ -0,0 +1,38 @@
+'use strict';
+
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// const mapEl = document.querySelector('#map');
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// console.log('mapEl', mapEl);
+
+navigator.geolocation.getCurrentPosition(
+  function (pos) {
+    console.log('pos', pos.coords);
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function (err) {
+    alert('Cannot get your current position');
+  }
+);
