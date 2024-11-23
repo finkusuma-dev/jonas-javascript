f356a5c5ead823f2cb605896199f54302d13090b
Arifin Kusuma <finkusuma.dev@gmail.com>
2024-11-23 16:39:02 +0700

    chore(searchResultsView/generateMarkupResult): remove user recipe and active link class

--- 18-forkify/src/js/views/searchResultsView.ts
+++ 18-forkify/src/js/views/searchResultsView.ts
@@ -108,18 +108,13 @@ class SearchResultsView {
   #generateMarkupResult(recipe: model.Recipe) {
     return `
       <li class="preview">
-        <a class="preview__link preview__link--active" href="#${recipe.id}">
+        <a class="preview__link" href="#${recipe.id}">
           <figure class="preview__fig">
             <img src="${recipe.image}" alt="${recipe.title}" />
           </figure>
           <div class="preview__data">
             <h4 class="preview__title">${recipe.title}</h4>
-            <p class="preview__publisher">${recipe.publisher}</p>
-            <div class="preview__user-generated">
-              <svg>
-                <use href="${icons}#icon-user"></use>
-              </svg>
-            </div>
+            <p class="preview__publisher">${recipe.publisher}</p>            
           </div>
         </a>
       </li>`;
